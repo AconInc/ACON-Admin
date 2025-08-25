31188307d915e26e011f22fc2cc6898b0a6075f5
Sumin Lee <cirtuare@gmail.com>
2025-08-25 18:16:46 +0900

    [Fix] 배포 오류 수정 (#11)

---
+++ acon-admin/src/app/dashboard/store-management/all-store/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+
+export default function AllStorePage() {
+  return (
+    <div style={{ padding: '24px' }}>
+      <h1>AllStore</h1>
+      <p>전체 장소 페이지입니다.</p>
+    </div>
+  )
+}
---
+++ acon-admin/src/app/dashboard/store-management/new-store/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+
+export default function NewStorePage() {
+  return (
+    <div style={{ padding: '24px' }}>
+      <h1>NewStore</h1>
+      <p>신규 장소 페이지입니다.</p>
+    </div>
+  )
+}
